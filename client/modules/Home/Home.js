f274b8f1da37ee5aadd08ca91a5a18246473fd81
Patryk Stanek <p.stanek3@gmail.com>
2019-06-30 13:32:42 +0200

    Better navigation, homepage and about page styling

--- client/modules/Home/Home.js
+++ client/modules/Home/Home.js
@@ -10,8 +10,8 @@ class Home extends Component {
   render() {
     return (
       <article className={styles['text-container']}>
-        <h2 className={styles['header']}>Homepage</h2>
-        <p className={styles['paragraph']}>
+        <h2 className={styles.header}>Homepage</h2>
+        <p className={styles.paragraph}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur luctus sapien eu finibus. Fusce non lacus ut orci luctus vulputate sed eu tortor. Nunc auctor augue ultrices ligula iaculis rhoncus a ac est. Sed dolor nunc, auctor rutrum ex ac, eleifend accumsan sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut a ultricies ante. In tincidunt odio eget scelerisque porttitor. Aliquam ac viverra nibh. Fusce sit amet elementum erat. Cras pretium quam in mauris faucibus, non rhoncus elit pulvinar. Suspendisse potenti.
         </p>
       </article>
